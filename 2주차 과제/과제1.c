5e7c3727236cba2181d5e2bfe4dddc54720aad45
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-03-18 14:05:03 +0900

    2주차 과제 1, 2 작성

---
+++ 2주차 과제/과제1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	char *ptr1[4] = { "korea","seoul","mapo","152¹øÁö 2/3" };
+
+	for (int i = 0; i < 4; i++) {
+		printf("%s\n", ptr1[i]);
+	}
+	printf("\n\n");
+
+	ptr1[2] = "Jongno";
+
+	for (int i = 0; i < 4; i++) {
+		printf("%s\n", ptr1[i]);
+	}
+
+	return 0;
+}
